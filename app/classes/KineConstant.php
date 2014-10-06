b683aa7d6f3a23a43630966e8b891fe5fe607b98
pitrack <innova@crunchbang-pitrack>
2014-10-06 11:34:04 +0200

    - new methods - new urls

---
+++ app/classes/KineConstant.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Description of KineConstant
+ *
+ * @author innova
+ */
+class KineConstant {
+
+    /**
+     * 
+     */
+    const IMAGE_TYPE_COLOR = "color";
+    const IMAGE_TYPE_DRAWING = "drawing";
+
+    /**
+     * 
+     */
+    const SOUND_TYPE_PHONEM = "phonem";
+    const SOUND_TYPE_WORD = "word";
+    const SOUND_TYPE_SENTENCE = "sentence";
+
+    /**
+     * 
+     */
+    const TEXT_TYPE_PHONETIC = "phonetic";
+    const TEXT_TYPE_WORD = "word";
+    const TEXT_TYPE_SENTENCE = "sentence";
+
+    
+}
